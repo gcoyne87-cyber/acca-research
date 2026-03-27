287b5da3289b85872bd9e0d20bd131a804f9e740
gcoyne87-cyber <gcoyne87@gmail.com>
2026-03-27 22:46:06 +0000

    Update fixtures.js

--- netlify/functions/fixtures.js
+++ netlify/functions/fixtures.js
@@ -3,7 +3,7 @@ const https = require('https');
 exports.handler = async function(event) {
   const API_KEY = '4876fc6e82fa43210ee8dc53214f000d';
   const { league, date, season } = event.queryStringParameters || {};
-  const url = 'https://v3.football.api-sports.io/fixtures?league=' + league + '&date=' + date + '&season=' + season;
+  const url = 'https://v3.football.api-sports.io/fixtures?league=' + league + '&date=' + date + '&season=' + '2025';
 
   return new Promise((resolve) => {
     const req = https.get(url, {
